3c075a5282befece02b62e0a42cf01a9b0aeb403
AlexeyK <avkhatskevich@gmail.com>
2017-03-16 18:03:54 +0300

    Recursion turned around

--- 2/2_1.js
+++ 2/2_1.js
@@ -1,24 +1,14 @@
 "use strict";
 function toBinaryString(val){
-  function recursiveToBinaryString(val, currentString){
-    var nextSymbol;
-    currentString = currentString || "";
-    if (val === 1){
-      return "1" + currentString;
-    }else if (val === 0){
-      return currentString;
-    }
-    // could be replaced with
-    // if (val <= 1 ) return (val===1? "1" : "") + currentString
-    // it seems ugly!
-    return recursiveToBinaryString(val >> 1 /*parseInt(number / 2)*/,
-        (val % 2 === 1 ? "1" : "0") + currentString)
-  }
-  if (val === 0){
-    return "0";
+  function recursiveToBinaryString(val){
+    if (val <= 1) return val + ""
+    return recursiveToBinaryString(val >> 1 /*parseInt(number / 2)*/) +
+        (val % 2 === 1 ? "1" : "0");
   }
   return recursiveToBinaryString(val);
 }
-
+console.log(toBinaryString(0));
+console.log(toBinaryString(1));
+console.log(toBinaryString(9));
 console.log(toBinaryString(90));
 
